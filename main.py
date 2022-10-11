baa9efef2810514c081a286ecf4f07ae2668829a
Fiona <100845392+fkit00@users.noreply.github.com>
2022-10-11 12:34:13 +0000

    Have now put my functions in an dictionary - now a matter of passing down input variable into dictionary

--- main.py
+++ main.py
@@ -3,3 +3,35 @@
 #take in second number 
 #then return a calucation
 #ask if they want to continue calculation with the result or press 'n' to start a new calculation- n will clear the screen 
+#for y we want to call whatevers next with the first thing
+
+import art
+
+
+print(art.logo)
+print('welcome to my calculator')
+
+def multiply(a,b):
+  return a*b
+
+def divide(a,b):
+  return a/b
+
+def add(a,b):
+  return a+b
+
+def minus(a,b):
+ return a-b
+
+
+
+
+operations={
+  "+":add,
+  "-":minus,
+  "*": multiply,
+  "/":divide
+}
+
+function= operations["+"]
+print(function(2,3))
