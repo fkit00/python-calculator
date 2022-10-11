ee7661177856381f1eeea155c65fdabb913263e8
Fiona <100845392+fkit00@users.noreply.github.com>
2022-10-11 11:16:02 +0000

    made a plan

---
+++ main.py
@@ -0,0 +1,5 @@
+# we want to take in the first number
+# pick an operator 
+#take in second number 
+#then return a calucation
+#ask if they want to continue calculation with the result or press 'n' to start a new calculation- n will clear the screen 
