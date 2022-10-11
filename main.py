5f5f1ab8a2dd016549bf0d78ca6f1802c8af37c5
Fiona <100845392+fkit00@users.noreply.github.com>
2022-10-11 14:16:48 +0000

    done the first bit (the first calculation)

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-# we want to take in the first number
-# pick an operator 
-#take in second number 
-#then return a calucation
-#ask if they want to continue calculation with the result or press 'n' to start a new calculation- n will clear the screen 
-#for y we want to call whatevers next with the first thing
-
 import art
-
-
 print(art.logo)
-print('welcome to my calculator')
 
 def multiply(a,b):
   return a*b
@@ -32,6 +22,20 @@ operations={
   "*": multiply,
   "/":divide
 }
+# we want to take in the first number
+print('welcome to my calculator')
+first_number=int(input('Please enter your first number '))
+# pick an operator 
+operate=input('Please pick an operator ')                  
+#take in second number 
+second_number=int(input('Please enter your second number '))
+#then return a calucation
+function= operations[operate]
+print(function(first_number, second_number))
+#ask if they want to continue calculation with the result or press 'n' to start a new calculation- n will clear the screen 
+#for y we want to call whatevers next with the first thing
 
-function= operations["+"]
-print(function(2,3))
+
+
+
+
